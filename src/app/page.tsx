e6097926cf3e4ce5062932a7e5566974ed750143
Shubham Nikam <nikamshubham224@gmail.com>
2024-12-30 19:20:38 +0530

    Setting up Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 
 
 export default async function Home() {
-  return <h1 className="text-red-600">Hellow world</h1>
+  return  <Button>Click ME</Button>
 }

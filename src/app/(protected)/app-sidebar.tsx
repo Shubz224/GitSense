4e259920331263e1b701e853071047a122e5a4be
Shubham Nikam <nikamshubham224@gmail.com>
2025-01-03 18:12:29 +0530

    dot error in dashboard solved

--- src/app/(protected)/app-sidebar.tsx
+++ src/app/(protected)/app-sidebar.tsx
@@ -1,65 +1,70 @@
 'use client'
 
-import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarInset, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { cn } from "@/lib/utils";
 import { Bot, CreditCard, LayoutDashboard, Presentation } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const items = [
     {
-        title:"Dashboard",
-        url:"/dashboard",
+        title: "Dashboard",
+        url: "/dashboard",
         icon: LayoutDashboard
     },
     {
-        title:"Q&A",
-        url:"/qa",
+        title: "Q&A",
+        url: "/qa",
         icon: Bot,
     },
     {
-        title:"Meetings",
-        url:"/meetings",
+        title: "Meetings",
+        url: "/meetings",
         icon: Presentation,
     },
     {
-        title:"Billing",
-        url:"/billing",
+        title: "Billing",
+        url: "/billing",
         icon: CreditCard,
     }
 
 ]
 
 
-export function AppSidebar(){
+export function AppSidebar() {
     const pathname = usePathname()
     return (
-       <Sidebar collapsible="icon" variant="floating">
-        <SidebarHeader>
-            Logo
-        </SidebarHeader>
-        <SidebarContent>
-            <SidebarGroup>
-                <SidebarGroupLabel>
-                    Application
-                </SidebarGroupLabel>
-                <SidebarGroupContent>
- {items.map(item=>{
-    return(
-        <SidebarMenuItem  key={item.title}>
- <SidebarMenuButton asChild>
-      <Link href={item.url} className="{cn({
-      '!bg-primary !text-white':pathname === item.url'})}">
-        <item.icon/>
-        <span>{item.title}</span>
-      </Link>
- </SidebarMenuButton>
-        </SidebarMenuItem>
-    )
- })}
-                </SidebarGroupContent>
-            </SidebarGroup>
-        </SidebarContent>
-       </Sidebar>
+        <Sidebar collapsible="icon" variant="floating">
+            <SidebarHeader>
+                Logo
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupLabel>
+                        Application
+                    </SidebarGroupLabel>
+                    <SidebarGroupContent>
+                       <SidebarMenu>
+                         
+                       {items.map(item => {
+                            return (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <Link href={item.url} className={cn({
+      '!bg-primary !text-white':pathname === item.url},'list-none')}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            )
+                        })}
+                       </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+        </Sidebar>
     )
 }
-export  default AppSidebar;
+
+export default AppSidebar

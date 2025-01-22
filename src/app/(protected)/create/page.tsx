afb515772056bce7c213ba9626fef053ec984536
Shubham Nikam <nikamshubham224@gmail.com>
2025-01-22 11:07:09 +0530

    Added Input Section and Buttons for gihub Repo and Tokens

--- src/app/(protected)/create/page.tsx
+++ src/app/(protected)/create/page.tsx
@@ -1,7 +1,9 @@
 'use client'
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+
 type FormInput = {
     repoUrl: string
     projectName: string
@@ -11,13 +13,13 @@ type FormInput = {
 const CreatePage = () => {
     const { register, handleSubmit, reset } = useForm<FormInput>()
     function onSubmit(data: FormInput) {
-        window.alert(data)
+        window.alert(JSON.stringify(data,null,2))
         return true;
 
     }
     return (
         <div className='flex items-centre gap-12 h-full justify-center'>
-            
+
             <div>
                 <div>
                     <h1 className='font-semibold text-2xl'>
@@ -32,11 +34,18 @@ const CreatePage = () => {
 
                 <div>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <Input  {...register('repoUrl',{ required: true })} placeholder='Project Name'
+                        <Input  {...register('projectName', { required: true })} placeholder='Project Name'
                             required />
-
-                            <div className='h-2'></div>
-                            
+                           <div className="h-2"></div>
+                           <Input  {...register('repoUrl', { required: true })} placeholder='Github URL'
+                            required />
+                           <div className="h-2"></div>
+                           <Input  {...register('githubToken', { required: true })} placeholder='Github Token (Optional)'
+                            required />
+                            <div className="h-4"></div>
+                            <Button type='submit'>
+                                Create Project
+                            </Button>
 
                     </form>
                 </div>

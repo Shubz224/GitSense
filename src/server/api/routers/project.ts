d18bec7a95c2eb9dda7cfeb9c455951daf0fdd7c
Shubham Nikam <nikamshubham224@gmail.com>
2025-02-02 14:38:16 +0530

    added save question , archiveProject query

--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -55,7 +55,44 @@ export const projectRouter = createTRPCRouter({
                 projectId: input.projectId
             }
         })
-    })
+    }),
 
+    saveAnswer: protectedProcedure.input(z.object(
+        {
+            projectId: z.string(),
+            question: z.string(),
+            filesReferences: z.any(),
+            answer: z.string(),
+
+
+        }
+    )).mutation(async ({ ctx, input }) => {
+        return await ctx.db.question.create({
+            data: {
+                answer: input.answer,
+                projectId: input.projectId,
+                filesReferences: input.filesReferences,
+                question: input.question,
+                userId: ctx.user.userId!
+            }
+        })
+    }),
+    getQuestion: protectedProcedure.input(z.object({ projectId: z.string() })).query(async ({ ctx, input }) => {
+        return await ctx.db.question.findMany({
+            where: {
+                projectId: input.projectId
+            },
+            include: {
+                user: true
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    }),
+
+    archiveProject: protectedProcedure.input(z.object({ projectId: z.string() })).mutation(async ({ ctx, input }) => {
+        return await ctx.db.project.update({ where: { id: input.projectId }, data: { deletedAt: new Date() } })
+    })
 
 });
